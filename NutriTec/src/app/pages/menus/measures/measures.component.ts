6d820f0d640c8b3d1062c892ab0772c5131db22b
adrian <56691299+AdrianTEC@users.noreply.github.com>
2021-11-09 11:44:31 -0600

    Measures component

--- NutriTec/src/app/pages/menus/measures/measures.component.ts
+++ NutriTec/src/app/pages/menus/measures/measures.component.ts
@@ -15,119 +15,37 @@ export class MeasuresComponent implements OnInit {
 
   measures: any[] = [
     {
-      date: '123',
-      measures: [
-        {
-          name: 'Peso',
-          value: 32,
-        },
-        {
-          name: 'IMC',
-          value: 32,
-        },
-        {
-          name: 'Músculo',
-          value: 62,
-        },
-        {
-          name: 'Grasa',
-          value: 44,
-        },
-        {
-          name: 'Grasa',
-          value: 44,
-        },
-        {
-          name: 'Cintura',
-          value: 44,
-        },
-        {
-          name: 'Cuello',
-          value: 34,
-        },
-        {
-          name: 'Caderas',
-          value: 124,
-        },
-      ],
+      date: '12/12/12',
+      peso: 32,
+      imc: 32,
+      musculo: 62,
+      grasa: 44,
+      cintura: 44,
+      cuello: 34,
+      caderas: 124,
     },
     {
-      date: '123',
-      measures: [
-        {
-          name: 'Peso',
-          value: 32,
-        },
-        {
-          name: 'IMC',
-          value: 32,
-        },
-        {
-          name: 'Músculo',
-          value: 62,
-        },
-        {
-          name: 'Grasa',
-          value: 44,
-        },
-        {
-          name: 'Grasa',
-          value: 44,
-        },
-        {
-          name: 'Cintura',
-          value: 44,
-        },
-        {
-          name: 'Cuello',
-          value: 34,
-        },
-        {
-          name: 'Caderas',
-          value: 124,
-        },
-      ],
-    },
-    {
-      date: '123',
-      measures: [
-        {
-          name: 'Peso',
-          value: 32,
-        },
-        {
-          name: 'IMC',
-          value: 32,
-        },
-        {
-          name: 'Músculo',
-          value: 62,
-        },
-        {
-          name: 'Grasa',
-          value: 44,
-        },
-        {
-          name: 'Grasa',
-          value: 44,
-        },
-        {
-          name: 'Cintura',
-          value: 44,
-        },
-        {
-          name: 'Cuello',
-          value: 34,
-        },
-        {
-          name: 'Caderas',
-          value: 124,
-        },
-      ],
+      date: '13/12/12',
+      peso: 36,
+      imc: 22,
+      musculo: 12,
+      grasa: 54,
+      cintura: 34,
+      cuello: 24,
+      caderas: 114,
     },
   ];
   today: boolean = true;
   history: boolean = false;
+
+  peso: number = 0;
+  imc: number = 0;
+  grasa: number = 0;
+  musculo: number = 0;
+
+  cintura: number = 0;
+  cuello: number = 0;
+  caderas: number = 0;
 
   //todayVars
 
@@ -139,22 +57,41 @@ export class MeasuresComponent implements OnInit {
   ngOnInit(): void {}
 
   submit() {
-    if (false) {
+    if (
+      this.peso > 0 &&
+      this.grasa > 0 &&
+      this.imc > 0 &&
+      this.musculo > 0 &&
+      this.caderas > 0 &&
+      this.cuello > 0 &&
+      this.caderas > 0 &&
+      this.cintura > 0
+    ) {
+      const data = {
+        date: new Date(),
+        peso: this.peso,
+        imc: this.imc,
+        grasa: this.grasa,
+        musculo: this.musculo,
+
+        cintura: this.cintura,
+        cuello: this.cuello,
+        caderas: this.caderas,
+      };
+      /**backend call here */
+
+      this.swal.showSuccess(
+        'Medición registrada!',
+        'Esta medición fue registrada con éxito'
+      );
+      this.me.close();
+    } else {
       this.swal.showError(
         'Error al ingresar los datos',
         'Datos ingresados insuficientes'
       );
       return;
     }
-
-    const data = {};
-    /**backend call here */
-
-    this.swal.showSuccess(
-      'Receta registrada!',
-      'Prontamente, un administrador se encargará de validar esta receta'
-    );
-    this.me.close();
   }
 
   openToday() {
@@ -166,5 +103,14 @@ export class MeasuresComponent implements OnInit {
     this.history = true;
   }
 
-  autoComplete() {}
+  autoComplete() {
+    this.peso = 70;
+    this.imc = 20;
+    this.grasa = 30;
+    this.musculo = 60;
+
+    this.cintura = 122;
+    this.cuello = 32;
+    this.caderas = 50;
+  }
 }
