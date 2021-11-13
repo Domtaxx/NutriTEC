cbc0f350d4b9ea4da6903fe4134ceb96ec7062c7
Lazh <luisandreyzuniga@gmail.com>
2021-11-12 22:37:58 -0600

    Updating AddProductToMenu Procedures
    Adding an user verification

--- SQL Server Scripts/store_procedures.sql
+++ SQL Server Scripts/store_procedures.sql
@@ -92,7 +92,7 @@ returns integer
     end
 GO
 
-
+drop PROCEDURE  dbo.GetPaymentAmount
 Create PROCEDURE dbo.GetPaymentAmount
 @email varchar(320)
 AS
@@ -133,38 +133,28 @@ BEGIN
 END
 GO
 
-Create Procedure dbo.spAddMenuToPlan
-@plan_name varchar(128),
-@email varchar(320),
-@menu_name varchar(128)
-AS
-    begin
-        declare @authorized as INTEGER;
-        select @authorized=count(*) from PLAN_ALIMENTACION
-        where Nombre=@plan_name and Correo_nutri=@email;
-        if(@authorized>0)begin
-            declare @defined as INTEGER;
-            select @defined=count(*) from MENU
-            where Nombre_plan_alimentacion=@plan_name and Nombre=@menu_name;
-            if(@defined=0)begin
-                insert into MENU(Nombre_plan_alimentacion, Nombre)
-                VALUES(@plan_name,@menu_name);
-            end
-        end
-    end
-GO
-
-
 Create Procedure dbo.spAddProductToMenu
 @plan_name varchar(128),
 @menu_name varchar(128),
-@codigo varchar(128)
+@codigo varchar(128),
+@correo varchar(320)
 AS
     begin
-        insert into MENU_PRODUCTO (nombre_plan_alimentacion, nombre_menu, codigo_barras)
-        values (@plan_name,@menu_name,@codigo);
-        select * from Producto_public
-        where Codigo_barras=@codigo;
+        declare @acceptedPlanUser as integer;
+        select @acceptedPlanUser=count(*) from PLAN_ALIMENTACION
+        WHERE Nombre=@plan_name and Correo_nutri=@correo;
+        if(@acceptedPlanUser>0)begin
+            declare @existingMenu as INTEGER;
+            select @existingMenu=count(*) from MENU
+            where Nombre_plan_alimentacion=@plan_name and Nombre=@menu_name;
+            if(@existingMenu>0)begin
+                insert into MENU_PRODUCTO (nombre_plan_alimentacion, nombre_menu, codigo_barras)
+                values (@plan_name,@menu_name,@codigo);
+                select * from Producto_public
+                where Codigo_barras=@codigo;
+            end
+            
+        end
+
     end
 Go
-
